64528a6bd77753c752088d961c13cce7894d07e5
jihoon <=>
2022-02-08 22:02:58 +0900

    05. @jsx

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* @jsx createElement */
 import { createDOM, createElement, render } from './react' 
 
 // 복잡해보인다. 개선방안
@@ -45,15 +46,30 @@ import { createDOM, createElement, render } from './react'
 // root가 하나인 트리구조이어야 한다. 따라서 하이요소 children은 배열로 받, 가변인자를 배열로 받으면 어떨까? 먼 말이지
 // 객체 리터럴과 createElement 비교
 // 코드 량을 조금 줄였다에 의의를 가져보자
-const vdom = createElement('p',{}, 
-createElement('h1', {}, "React 만들기"),
-createElement('ul', {}, 
-  createElement('li', { style: "color: red"}, "첫 번째 아이템"),
-  createElement('li', {style: "color: blue"}, "첫 번째 아이템"),
-  createElement('li', {style: "color: green"}, "첫 번째 아이템"),
-  )
-)
+// const vdom = createElement('p',{}, 
+// createElement('h1', {}, "React 만들기"),
+// createElement('ul', {}, 
+//   createElement('li', { style: "color: red"}, "첫 번째 아이템"),
+//   createElement('li', {style: "color: blue"}, "첫 번째 아이템"),
+//   createElement('li', {style: "color: green"}, "첫 번째 아이템"),
+//   )
+// );
 
-console.log(vdom);
+// 개선방안 : @babel @jsx 바벨이 제공해줌 transfiler
+// 1. 실제로 React 만들 때 DOM보다 훨씬 간단한 구조를 만들었다.
+// 2. 객체 리터럴, 함수호출 보다 구조 파악하기 쉽다 (마크업 형태를 유지하므로)
+/* React 스럽게 바뀌었음 
+제약 사항
+- 코드상으로는 creaetElement 사용하지 않지만 import 할 때 호출해야 하는 경우 있음
+*/
+const vdom = <p>
+  <h1>React 만들기</h1>
+  <ul>
+    <li style="color:red">첫 번째 아이템</li>
+    <li style="color:red">두 번째 아이템</li>
+    <li style="color:red">세 번째 아이템</li>
+  </ul>
+</p>
+
 
 render(vdom, document.querySelector('#root'))
--- src/react.js
+++ src/react.js
@@ -16,6 +16,7 @@ export function createDOM(node) {
 }
 
 export function createElement(tag, props, ...children) {
+  props = props || {} // default parameter 는 사용하지 못함 null값이 들어오므로
   return {
     tag,
     props,

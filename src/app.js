05f180faf95d09527b42c71a89fb2332464b4ff2
jihoon <=>
2022-02-14 20:54:05 +0900

    06.함수컴포넌트

--- src/app.js
+++ src/app.js
@@ -1,47 +1,57 @@
 /* @jsx createElement */
-import { createElement, render } from './react' 
+import { createDOM, createElement, render } from './react' 
 
-// 복잡해보인다. 개선방안
-// 객체 생성 함수 ... 세 개의 속성을 갖고 있는 객체를 생성하는 함수를 만든다면?
-// const vdom = {
-//   tag: 'p',
-//   props: {},
-//   children: [{
-//       tag: 'h1',
-//       props: {
-//       },
-//       children: ["React 만들기"],
-//     },
-//     {
-//       tag: 'ul',
-//       props: {},
-//       children: [{
-//           tag: 'li',
-//           props: {
-//             style : "color : blue"
-//           },
-//           children: ["첫 번째 아이템"]
-//         },
-//         {
-//           tag: 'li',
-//           props: {
-//             style: "color : green"
+/* 
+복잡해보인다. 개선방안
+객체 생성 함수 ... 세 개의 속성을 갖고 있는 객체를 생성하는 함수를 만든다면?
+const vdom = {
+  tag: 'p',
+  props: {},
+  children: [{
+      tag: 'h1',
+      props: {
+      },
+      children: ["React 만들기"],
+    },
+    {
+      tag: 'ul',
+      props: {},
+      children: [{
+          tag: 'li',
+          props: {
+            style : "color : blue"
+          },
+          children: ["첫 번째 아이템"]
+        },
+        {
+          tag: 'li',
+          props: {
+            style: "color : green"
 
-//           },
-//           children: ["두 번째 아이템"]
-//         },
-//         {
-//           tag: 'li',
-//           props: {
-//              style: "color : red"
+          },
+          children: ["두 번째 아이템"]
+        },
+        {
+          tag: 'li',
+          props: {
+             style: "color : red"
 
-//           },
-//           children: ["세 번째 아이템"]
-//         },
-//       ]
-//     }
-//   ],
-// };
+          },
+          children: ["세 번째 아이템"]
+        },
+      ]
+    }
+  ],
+}; 
+*/
+const vdomObjectLiteral = {
+  tag : 'div',
+  props: { style : "color: red", class : "test", id: "container"},
+  children : ["객체 리터럴 만들기"]
+}
+createDOM(vdomObjectLiteral);
+render(vdomObjectLiteral, document.querySelector('#root'));
+
 
 // root가 하나인 트리구조이어야 한다. 따라서 하이요소 children은 배열로 받, 가변인자를 배열로 받으면 어떨까? 먼 말이지
 // 객체 리터럴과 createElement 비교
@@ -62,14 +72,16 @@ import { createElement, render } from './react'
 제약 사항
 - 코드상으로는 creaetElement 사용하지 않지만 import 할 때 호출해야 하는 경우 있음
 */
-// const vdom = <p>
-//   <h1>React 만들기</h1>
-//   <ul>
-//     <li style="color:red">첫 번째 아이템</li>
-//     <li style="color:red">두 번째 아이템</li>
-//     <li style="color:red">세 번째 아이템</li>
-//   </ul>
-// </p>
+/* 
+const vdom = <p>
+  <h1>React 만들기</h1>
+  <ul>
+    <li style="color:red">첫 번째 아이템</li>
+    <li style="color:red">두 번째 아이템</li>
+    <li style="color:red">세 번째 아이템</li>
+  </ul>
+</p> 
+*/
 
 /* 사용자 컴포넌트를 만드는 방법 제공
 함수 컴포넌트 혹은 클래스 컴포넌트
@@ -82,6 +94,7 @@ import { createElement, render } from './react'
 무조건 대문자로 시작해야 한다. 컴포넌트 첫글자는
 대문자는 함수든 클래스든 둘 중 하나가 들어올 것이다 하는 Logic (전제)
 */
+// 함수 컴포넌트의 개념 - React의 기저에 깔려있는 핵심코드 이해해보자
 function Title(props, children) {
   console.log('여기는 타이틀')
   return <h1>{props.children}</h1>;
@@ -98,4 +111,4 @@ const vdom = <p>
   </ul>
 </p>
 
-render(vdom, document.querySelector('#root'))
+render(vdom, document.querySelector('#root'));
--- src/react.js
+++ src/react.js
@@ -18,13 +18,15 @@ export function createDOM(node) {
 export function createElement(tag, props, ...children) {
   props = props || {} // default parameter 는 사용하지 못함 null값이 들어오므로
 
+  // tag가 함수로 들어온다면..? tag의 이름이 대문자로 시작되면 이것을 자바스크립트 값으로 취급하고 반드시 함수이어야 한다. 그 함수는 jsx를 반환해야 할 것
+  // 따라서 대문자로 시작해야 한다. 컴포넌트는 무조건 대문자로 시작해야 한다!! *(중요)*
   if (typeof tag === 'function') {
-    if (children.length > 0) { 
+    if (children.length > 0) { // 자식 요소가 있다면
       return tag({
-        ...props,
-        children: children.length === 1 ? children[0] : children,
+        ...props, // props는 그대로 전달
+        children: children.length === 1 ? children[0] : children, // 하나일 때에는 단순 값으로 예외처리
       })
-    } else {
+    } else { // 자식 요소가 없으면
       return tag(props); // 어렵다
     }
   } else {
